ccd70fb1271e1752f517257549161c8bf40ea427
justinbartlett9 <coldmilk19@gmail.com>
2021-10-13 17:12:05 -0400

    added to travis.yml

---
+++ build/precache-manifest.9859056a38054d0e0cffafae2e3d388c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18daa2f00dff13a44b9b",
+    "url": "/justinbartlett9/COMP3104/static/css/main.9c008f63.chunk.css"
+  },
+  {
+    "revision": "18daa2f00dff13a44b9b",
+    "url": "/justinbartlett9/COMP3104/static/js/main.18daa2f0.chunk.js"
+  },
+  {
+    "revision": "eb2c1bfd941293477e0d",
+    "url": "/justinbartlett9/COMP3104/static/js/runtime~main.eb2c1bfd.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/justinbartlett9/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "c3d73669636767ce4752b58cac783e31",
+    "url": "/justinbartlett9/COMP3104/index.html"
+  }
+];
